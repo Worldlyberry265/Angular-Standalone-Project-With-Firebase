3bddeda9e68432e482d521f699afaad13073de5e
Baraa EL Ghalayini <74971527+Worldlyberry265@users.noreply.github.com>
2024-05-10 19:46:40 +0300

    Add files via upload

---
+++ CourseProject - Optimized/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    firbaseAPIKey: 'AIzaSyDIDHCGcaD-zPEdkpiydSfW-TcP8bI3A4E',
+};
---
+++ CourseProject - Optimized/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    firbaseAPIKey: 'AIzaSyDIDHCGcaD-zPEdkpiydSfW-TcP8bI3A4E',
+};
